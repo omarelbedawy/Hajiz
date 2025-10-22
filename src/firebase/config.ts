c2b0ec32ab157094eab6440d46aa8f6ddc1b5373
Omar Elbedawy <elbedawyomar2009@gmail.com>
2025-10-22 18:15:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7396868400-6be71",
+  "appId": "1:1024974352389:web:d9f4190279f1a1dec76c6c",
+  "apiKey": "AIzaSyAbZFn5inTFiwt0aVI9n9SeXyKWQxpSIFI",
+  "authDomain": "studio-7396868400-6be71.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1024974352389"
+};
